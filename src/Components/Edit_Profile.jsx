cdcf60e227e3e714de60a90c12a4ae36bce5c908
samio11 <samiohasan6@gmail.com>
2023-11-21 18:43:00 +0600

    Live Link Added

--- src/Components/Edit_Profile.jsx
+++ src/Components/Edit_Profile.jsx
@@ -4,7 +4,7 @@ const Edit_Profile = () => {
     return (
         <div>
             <div className='h-[85%] w-full flex justify-center items-center'>
-               <img src="../../public/Authore_Problem.jpg" alt="" />
+               <img src="/Authore_Problem.jpg" alt="" />
             </div>
             <div className='w-full h-[15%] flex justify-center items-center'>
                   <p className='font-semibold text-2xl'>Authore Will Make This Section Later</p>
--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -4,7 +4,7 @@ const Profile = () => {
     return (
         <div className='w-full h-full'>
             <div className='w-full h-[50%] flex justify-center items-center bg-[radial-gradient(circle at -8.9% 51.2%, rgb(255, 124, 0) 0%, rgb(255, 124, 0) 15.9%, rgb(255, 163, 77) 15.9%, rgb(255, 163, 77) 24.4%, rgb(19, 30, 37) 24.5%, rgb(19, 30, 37) 66%);]'>
-                <img src="../../public/samio.jpg" className='h-[200px] lg:h-[350px] w-[200] lg:w-[350px] rounded-full' alt="" />
+                <img src="samio.jpg" className='h-[200px] lg:h-[350px] w-[200] lg:w-[350px] rounded-full' alt="" />
             </div>
             <div className='w-full h-[50%] flex'>
                 <div className='flex-1 flex justify-center items-center'>
@@ -17,7 +17,7 @@ const Profile = () => {
                 </div>
                 <div className='flex-1 flex justify-center items-center'>
                      <div>
-                     <a href="https://www.aiub.edu/"><img src="../../public/American_International_University-Bangladesh_Monogram.svg.png" className='w-[100px] lg:w-[200px] h-[100px] lg:h-[200px]' alt="" /></a>
+                     <a href="https://www.aiub.edu/"><img src="American_International_University-Bangladesh_Monogram.svg.png" className='w-[100px] lg:w-[200px] h-[100px] lg:h-[200px]' alt="" /></a>
                      </div>
                 </div>
             </div>
